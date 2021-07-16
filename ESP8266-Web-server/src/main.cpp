55070a3b62f0ef511eddad92e33dcddf2586bab8
arctic-cheetah <gondonlondon@gmail.com>
2021-07-16 11:47:35 +1000

    added extra GPIO pin

--- ESP8266-Web-server/src/main.cpp
+++ ESP8266-Web-server/src/main.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-
 /*********
   Rui Santos
   Complete project details at https://randomnerdtutorials.com  
@@ -19,13 +18,15 @@ WiFiServer server(80);
 String header;
 String html = "";
 
-// Auxiliar variables to store the current output state
+// Auxiliary variables to store the current output state
 String output5State = "off";
 String output4State = "off";
+String output14State = "off";
 
 // Assign output variables to GPIO pins
 const int output5 = 5;
 const int output4 = 4;
+const int output14 = 14;
 
 // Current time
 unsigned long currentTime = millis();
@@ -33,15 +34,22 @@ unsigned long currentTime = millis();
 unsigned long previousTime = 0; 
 // Define timeout time in milliseconds (example: 2000ms = 2s)
 #define TIMEOUT 2000
+////////////////////////////////////////////////////////
+//Function declarations
+void send_html(WiFiClient client);
+
+////////////////////////////////////////////////////////
 
 void setup() {
   Serial.begin(115200);
   // Initialize the output variables as outputs
   pinMode(output5, OUTPUT);
   pinMode(output4, OUTPUT);
+  pinMode(output14, OUTPUT);
   // Set outputs to LOW
   digitalWrite(output5, LOW);
   digitalWrite(output4, LOW);
+  digitalWrite(output14, LOW);
 
   // Connect to Wi-Fi network with SSID and password
   Serial.print("Connecting to ");
@@ -67,81 +75,26 @@ void loop(){
     String currentLine = "";                // make a String to hold incoming data from the client
     currentTime = millis();
     previousTime = currentTime;
-    while (client.connected() && (currentTime - previousTime <= TIMEOUT)) { // loop while the client's connected
+    bool end_connection = false;
+    // loop while the client's connected
+    // Timeout is needed to prevent client from waiting too long
+    while (client.connected() && !end_connection && (currentTime - previousTime <= TIMEOUT)) { 
+      
       currentTime = millis();         
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.print(c);                    // print it out the serial monitor
+      if (client.available()) {
+        // read and print bytes from the client             
+        char c = client.read();             
+        Serial.print(c);                    
         header += c;
-        if (c == '\n') {                    // if the byte is a newline character
+        if (c == '\n') {                    
+          // if the byte is a newline character
           // if the current line is blank, you got two newline characters in a row.
           // that's the end of the client HTTP request, so send a response:
           if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /5/on") >= 0) {
-              Serial.println("GPIO 5 on");
-              output5State = "on";
-              digitalWrite(output5, HIGH);
-            } else if (header.indexOf("GET /5/off") >= 0) {
-              Serial.println("GPIO 5 off");
-              output5State = "off";
-              digitalWrite(output5, LOW);
-            } else if (header.indexOf("GET /4/on") >= 0) {
-              Serial.println("GPIO 4 on");
-              output4State = "on";
-              digitalWrite(output4, HIGH);
-            } else if (header.indexOf("GET /4/off") >= 0) {
-              Serial.println("GPIO 4 off");
-              output4State = "off";
-              digitalWrite(output4, LOW);
-            }
-            
-            // Display the HTML web page
-            html += "<!DOCTYPE html><html>";
-            html += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
-            html += "<link rel=\"icon\" href=\"data:,\">";
-            // CSS to style the on/off buttons 
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            html += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}";
-            html += ".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}";
-            html += ".button:hover {background-color: #358ca0;}.button2 {background-color: #4b5355;}.button2:hover {background-color: #77878A;}";
-            html += "</style></head>";
-            
-            // Web Page Heading
-            html += "<body><h1>ESP8266 Web Server</h1>";
-            
-            // Display current state, and ON/OFF buttons for GPIO 5  
-            html += "<p>GPIO 5 - State " + output5State + "</p>";
-            // If the output5State is off, it displays the ON button       
-            if (output5State=="off") {
-              html += "<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>";
-            } else {
-              html += "<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>";
-            } 
-               
-            // Display current state, and ON/OFF buttons for GPIO 4  
-            html += "<p>GPIO 4 - State " + output4State + "</p>";
-            // If the output4State is off, it displays the ON button       
-            if (output4State=="off") {
-              html += "<p><a href=\"/4/on\"><button class=\"button\">ON</button></a></p>";
-            } else {
-              html += "<p><a href=\"/4/off\"><button class=\"button button2\">OFF</button></a></p>";
-            }
-            html += "</body></html>";
-            client.println(html);
-            // The HTTP response ends with another blank line
-            client.println();
-            html="";
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
+            send_html(client);
+            end_connection = true;
+          } 
+          else { // if you got a newline, then clear currentLine
             currentLine = "";
           }
         } else if (c != '\r') {  // if you got anything else but a carriage return character,
@@ -156,4 +109,99 @@ void loop(){
     Serial.println("Client disconnected.");
     Serial.println("");
   }
+  //To Do: Strobe LED
+
+}
+
+////////////////////////////////////////////////////////
+//functions
+
+//Send data to the client
+void send_html(WiFiClient client) {
+
+  // IMPORTANT: this is a response code to the client
+  // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+  // and a content-type so the client knows what's coming, then a blank line:
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-type:text/html");
+  client.println("Connection: close");
+  client.println();
+  
+  // turns the GPIOs on and off
+  if (header.indexOf("GET /5/on") >= 0) {
+    Serial.println("GPIO 5 on");
+    output5State = "on";
+    digitalWrite(output5, HIGH);
+  } else if (header.indexOf("GET /5/off") >= 0) {
+    Serial.println("GPIO 5 off");
+    output5State = "off";
+    digitalWrite(output5, LOW);
+  } else if (header.indexOf("GET /4/on") >= 0) {
+    Serial.println("GPIO 4 on");
+    output4State = "on";
+    digitalWrite(output4, HIGH);
+  } else if (header.indexOf("GET /4/off") >= 0) {
+    Serial.println("GPIO 4 off");
+    output4State = "off";
+    digitalWrite(output4, LOW);
+  } else if (header.indexOf("GET /14/on") >= 0) {
+    Serial.println("GPIO 14 on");
+    output14State = "on";
+    digitalWrite(output14, HIGH);
+  } else if (header.indexOf("GET /14/off") >= 0) {
+    Serial.println("GPIO 14 off");
+    output14State = "off";
+    digitalWrite(output14, LOW);
+  }
+
+  
+  // Display the HTML web page
+  html += "<!DOCTYPE html><html>";
+  html += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
+  html += "<link rel=\"icon\" href=\"data:,\">";
+  // CSS to style the on/off buttons 
+  // Feel free to change the background-color and font-size attributes to fit your preferences
+  html += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}";
+  html += ".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}";
+  html += ".button:hover {background-color: #358ca0;}.button2 {background-color: #4b5355;}.button2:hover {background-color: #77878A;}";
+  html += "</style></head>";
+  
+  // Web Page Heading
+  html += "<body><h1>ESP8266 Web Server</h1>";
+  
+  // Display current state, and ON/OFF buttons for GPIO 5  
+  html += "<p>GPIO 5(D1) - State " + output5State + "</p>";
+  // If the output5State is off, it displays the ON button       
+  if (output5State=="off") {
+    html += "<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>";
+  } else {
+    html += "<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>";
+  } 
+      
+  // Display current state, and ON/OFF buttons for GPIO 4  
+  html += "<p>GPIO 4(D2) - State " + output4State + "</p>";
+  // If the output4State is off, it displays the ON button       
+  if (output4State=="off") {
+    html += "<p><a href=\"/4/on\"><button class=\"button\">ON</button></a></p>";
+  } else {
+    html += "<p><a href=\"/4/off\"><button class=\"button button2\">OFF</button></a></p>";
+  }
+
+  //Display current state of D5 (GPIO 14)
+  html += "<p>GPIO 14(D5) - State " + output14State + "</p>";
+  // If the output4State is off, it displays the ON button  
+  if (output14State == "off") {
+    html += "<p><a href=\"/14/on\"><button class=\"button\">ON</button></a></p>";
+  }
+  else {
+    html += "<p><a href=\"/14/off\"><button class=\"button\">OFF</button></a></p>";
+  }
+
+  //html += "<p>GPIO 4(D2) - Strobing off</p><p><a href=\"flash/off\"><button class=\"button button2\">OFF</button></a></p>";
+  html += "</body></html>";
+
+  client.println(html);
+  // The HTTP response ends with another blank line
+  client.println();
+  html="";
 }
